7a6cee90d48eceebdb890d2d6b16d37a5fe8e0e1
Nitish Harsoor <55267125+nitishkmr005@users.noreply.github.com>
2020-10-31 15:07:34 +0530

    new

--- Code/chatbot_database.py
+++ Code/chatbot_database.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 from datetime import datetime
 
-timeframe = '2009-05'
+timeframe = '2006-03'
 sql_transaction = []
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
@@ -10,11 +10,6 @@ c = connection.cursor()
 
 def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
-
-if __name__ == '__main__':
-    create_table()
-    row_counter = 0
-    paired_rows = 0
 
 def format_data(data):
     data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
@@ -32,14 +27,21 @@ def find_parent(pid):
         #print(str(e))
         return False
 
-    with open("C:\Users\nitis\OneDrive\Jupyter Notebooks\My Notebooks\1.8. My GitHub Repo\NLP-Chatbot_1\data\RC_2006-03", buffering=1000) as f:
+if __name__ == '__main__':
+    create_table()
+    row_counter = 0
+    paired_rows = 0
+
+    print("Inside main")
+    with open(r"C:\Users\nitis\OneDrive\Jupyter Notebooks\My Notebooks\My_GitHub_Repo\NLP-Chatbot_1\data\RC_2006-03", buffering=1000) as f:
         for row in f:
-            print(row)
+            #print(row)
             row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
             body = format_data(row['body'])
             created_utc = row['created_utc']
             score = row['score']
-            comment_id = row['name']
             subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+        print(row_counter)
