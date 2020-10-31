aee5d8e123860b85600f357dff39264ba7b3a299
Nitish Harsoor <55267125+nitishkmr005@users.noreply.github.com>
2020-10-31 14:36:58 +0530

    new

--- atom_github_test.py
+++ atom_github_test.py
@@ -2,4 +2,11 @@
 #CNTL+SHIFT+P => Find => Search for github clone
 #CNTL+SHIFT+B => Run
 #Get token from https://github.atom.io/login
+
+#Go the Setting-> Packages -> In Search Bar, search for the platformio.
+#Open platformio and go to Shell Override Option.
+#Put the following line as per as your Terminal Requirement
+#For cmd- C:\\WINDOWS\\System32\\cmd.exe
+#For Power Shell- C:\\WINDOWS\\Sysnative\\WndowsPowerShell\\v1.0\\powershell.exe
+#For bash- C:\\Program Files\\Git\\bin\\bash.exe or C:\\Windows\\System32\\bash.exe
 print("Atom and Github test")
